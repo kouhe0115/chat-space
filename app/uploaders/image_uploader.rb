f83177cff953842bfe3f1eb8798b5072b17e5a04
kouhe0115 <k.soccer0115@icloud.com>
2019-04-05 12:34:52 +0900

    Edit
    image_uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,4 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fit: [200, 200]
   storage :fog
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 end
